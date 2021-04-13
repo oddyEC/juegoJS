ad8e22e17583c62f9eeed3d54810ede6bb52a1ab
oddyec <diegomarquez2008@outlook.com>
2021-04-13 18:59:51 -0500

    juego final

--- juego.js
+++ juego.js
@@ -3,17 +3,21 @@ const violeta = document.getElementById("violeta");
 const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
+const ULTIMO_NIVEL = 10;
 
 class Juego {
   constructor() {
+    this.inicializar = this.inicializar.bind(this);
     this.inicializar();
     this.generarSecuencia();
-    this.siguienteNivel();
+    setTimeout(this.siguienteNivel, 500);
   }
 
   inicializar() {
-    btnEmpezar.classList.add("hide");
-    this.nivel = 2;
+    this.siguienteNivel = this.siguienteNivel.bind(this);
+    this.elegirColor = this.elegirColor.bind(this);
+    this.toggleBtnEmpezar();
+    this.nivel = 1;
     this.colores = {
       celeste,
       violeta,
@@ -22,15 +26,24 @@ class Juego {
     };
   }
 
+  toggleBtnEmpezar() {
+    if (btnEmpezar.classList.contains("hide")) {
+      btnEmpezar.classList.remove("hide");
+    } else {
+      btnEmpezar.classList.add("hide");
+    }
+  }
+
   generarSecuencia() {
-    this.secuencia = new Array(10)
+    this.secuencia = new Array(ULTIMO_NIVEL)
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
   }
 
   siguienteNivel() {
+    this.subnivel = 0;
     this.iluminarSecuencia();
-    this.agregarEventoClick();
+    this.agregarEventosClick();
   }
 
   transformarNumeroAColor(numero) {
@@ -46,8 +59,20 @@ class Juego {
     }
   }
 
+  transformarColorANumero(color) {
+    switch (color) {
+      case "celeste":
+        return 0;
+      case "violeta":
+        return 1;
+      case "naranja":
+        return 2;
+      case "verde":
+        return 3;
+    }
+  }
+
   iluminarSecuencia() {
-    // let mantiene la variable a diferencia de var que siempre pisa la misma variable, usar siempre conts antes que let y usar siepre let antes de var.
     for (let i = 0; i < this.nivel; i++) {
       const color = this.transformarNumeroAColor(this.secuencia[i]);
       setTimeout(() => this.iluminarColor(color), 1000 * i);
@@ -62,14 +87,52 @@ class Juego {
   apagarColor(color) {
     this.colores[color].classList.remove("light");
   }
-  agregarEventosClick(){
-    this.colores.celeste.addEventListener('click',this.elegirColor)
-    this.colores.verde.addEventListener('click',this.elegirColor)
-    this.colores.violeta.addEventListener('click',this.elegirColor)
-    this.colores.naranja.addEventListener('click',this.elegirColor)
+
+  agregarEventosClick() {
+    this.colores.celeste.addEventListener("click", this.elegirColor);
+    this.colores.verde.addEventListener("click", this.elegirColor);
+    this.colores.violeta.addEventListener("click", this.elegirColor);
+    this.colores.naranja.addEventListener("click", this.elegirColor);
   }
+
+  eliminarEventosClick() {
+    this.colores.celeste.removeEventListener("click", this.elegirColor);
+    this.colores.verde.removeEventListener("click", this.elegirColor);
+    this.colores.violeta.removeEventListener("click", this.elegirColor);
+    this.colores.naranja.removeEventListener("click", this.elegirColor);
+  }
+
   elegirColor(ev) {
-    console.log(ev)
+    const nombreColor = ev.target.dataset.color;
+    const numeroColor = this.transformarColorANumero(nombreColor);
+    this.iluminarColor(nombreColor);
+    if (numeroColor === this.secuencia[this.subnivel]) {
+      this.subnivel++;
+      if (this.subnivel === this.nivel) {
+        this.nivel++;
+        this.eliminarEventosClick();
+        if (this.nivel === ULTIMO_NIVEL + 1) {
+          this.ganoElJuego();
+        } else {
+          setTimeout(this.siguienteNivel, 1500);
+        }
+      }
+    } else {
+      this.perdioElJuego();
+    }
+  }
+
+  ganoElJuego() {
+    swal("Simon Dice", "Felicitaciones, ganaste el juego!", "success").then(
+      this.inicializar
+    );
+  }
+
+  perdioElJuego() {
+    swal("Simon  Dice", "Lo lamentamos, perdiste :(", "error").then(() => {
+      this.eliminarEventosClick();
+      this.inicializar();
+    });
   }
 }
 
